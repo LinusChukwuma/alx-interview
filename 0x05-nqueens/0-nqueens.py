40efa82db342f3ba8b563d919ccf9608b141263f
LinusChukwuma <104623011+LinusChukwuma@users.noreply.github.com>
2023-04-27 23:51:05 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,43 +1,67 @@
 #!/usr/bin/python3
+
 import sys
 
-def is_valid(board, row, col, N):
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at board[row][col] given the current
+    state of the board.
+    """
     # Check row on left side
     for i in range(col):
-        if board[row][i] == 1:
+        if board[row][i]:
             return False
-
     # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
+        if board[i][j]:
             return False
-
     # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j]:
             return False
-
     return True
 
-def solve_n_queens(board, col, N):
-    # Base case: all queens are placed
-    if col == N:
-        solution = []
-        for row in range(N):
-            for c in range(N):
-                if board[row][c] == 1:
-                    solution.append([row, c])
-        return [solution]
 
+def solve_n_queens(n):
+    """
+    Solve the n queens problem using backtracking.
+
+    Return a list of solutions.
+    Each solution is a list of row indices for each column index, e.g.
+    [0, 2, 4, 1, 3] represents the board:
+    Q _ _ _ _
+    _ _ Q _ _
+    Q _ _ _ _
+    _ Q _ _ _
+    _ _ Q _ _
+    """
+    board = [[0 for x in range(n)] for y in range(n)]
     solutions = []
-    for row in range(N):
-        if is_valid(board, row, col, N):
-            board[row][col] = 1
-            for solution in solve_n_queens(board, col + 1, N):
-                solutions.append(solution)
-            board[row][col] = 0
 
+    def backtrack_n_queens(col):
+        """
+        Recursively place queens on the board column by column.
+        When a solution is found (i.e. we have placed n queens), add it to the
+        list of solutions.
+        """
+        nonlocal board
+        if col == n:
+            # We have placed n queens (0, 1, ..., n-1) so we have a solution.
+            # Don't use board.copy() here since the inner lists are still shared
+            # and we'll modify the same list object in subsequent iterations.
+            solutions.append([row.index(1) for row in board])
+            return
+
+        for row in range(n):
+            if is_safe(board, row, col, n):
+                board[row][col] = 1
+                backtrack_n_queens(col + 1)
+                board[row][col] = 0
+
+    backtrack_n_queens(0)
     return solutions
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -45,16 +69,16 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0] * N for _ in range(N)]
-    solutions = solve_n_queens(board, 0, N)
+    solutions = solve_n_queens(n)
+
     for solution in solutions:
-        print(solution)
+        print([[i, j] for i, j in enumerate(solution)])
