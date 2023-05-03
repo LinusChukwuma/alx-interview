91186374e978ff52fb34523c9fa6e14103f1fac7
LinusChukwuma <104623011+LinusChukwuma@users.noreply.github.com>
2023-05-03 22:52:54 +0100

    Add files via upload

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-const movieId = process.argv[2];
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-// Step 2: Construct the URL for the SWAPI /films/{id} endpoint
-const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-// Step 3: Use the request module to fetch the movie data from the SWAPI
-request(movieUrl, function(error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-
-  // Step 4: Extract the list of characters from the movie data
-  const characterUrls = movieData.characters;
-
-  // Step 5: Use the request module to fetch the data for each character in the list
-  characterUrls.forEach(function(characterUrl) {
-    request(characterUrl, function(error, response, body) {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-
-      // Step 6: Extract the name of each character and print it to the console
-      console.log(characterData.name);
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
-});
+}
